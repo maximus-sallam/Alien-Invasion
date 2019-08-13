87a2ab03aa207c46a7501a47cd8d088c330baa8c
maximus-sallam <maximus.sallam@gmail.com>
2019-08-13 06:58:51 -0400

    added background color per page 242

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,7 +6,10 @@ def run_game():
     # Initialize game and create a screen object
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Vagina Invasion")
+    pygame.display.set_caption("Max Invasion")
+
+    # Set the background color.
+    bg_color = (25, 175, 255)
 
     # Start the main loop for the game.
     while True:
@@ -15,6 +18,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # Redraw the screen during each pass through the loop.
+        screen.fill(bg_color)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
