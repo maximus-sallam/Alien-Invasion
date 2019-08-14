0413e06933518de9212b3babcf95e38aa2888df8
maximus-sallam <maximus.sallam@gmail.com>
2019-08-14 06:55:12 -0400

    modified game_finctions.py and settings.py to fire the bullets with space bar

--- game_functions.py
+++ game_functions.py
@@ -1,17 +1,18 @@
 import sys
 import pygame
+from bullet import Bullet
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Respond to the keypresses."""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
@@ -21,6 +22,10 @@ def check_keydown_events(event, ship):
         ship.moving_down = True
     elif event.key == pygame.K_UP:
         ship.moving_up = True
+    elif event.key == pygame.K_SPACE:
+        # Create a new bullet and add it to the bullets group.
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     """Respond to ket releases."""
@@ -33,10 +38,13 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_UP:
         ship.moving_up = False
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
+    # Redraw all bullets behind ship and aliens.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
 
     # Make the most recently drawn screen visible.
--- settings.py
+++ settings.py
@@ -15,4 +15,4 @@ class Settngs():
         self.bullet_speed_factor = 10
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        self.bullet_color = 255, 255, 255
