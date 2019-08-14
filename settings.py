8a7e1f9568f0800effc3ce58a60e1ef3fadab757
maximus-sallam <maximus.sallam@gmail.com>
2019-08-14 07:08:41 -0400

    changed bullet color to red

--- settings.py
+++ settings.py
@@ -15,4 +15,4 @@ class Settngs():
         self.bullet_speed_factor = 15
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 255, 255, 255
+        self.bullet_color = 255, 0, 0
