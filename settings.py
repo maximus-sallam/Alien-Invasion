4e80a71d746e799c7756ea1f4c80df5f56914ffe
maximus-sallam <maximus.sallam@gmail.com>
2019-08-16 05:11:12 -0400

    made aliens move left and right accross screen and drop down when they hit the edge of the screen

--- settings.py
+++ settings.py
@@ -28,4 +28,7 @@ class Settngs():
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed_factor = 5
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
