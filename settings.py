ef5618f09b3434045382f58be0ee438710c9a4e2
maximus-sallam <maximus.sallam@gmail.com>
2019-08-16 05:43:22 -0400

    increased alien movement speed

--- settings.py
+++ settings.py
@@ -28,7 +28,7 @@ class Settngs():
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed_factor = 1
+        self.alien_speed_factor = 5
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
