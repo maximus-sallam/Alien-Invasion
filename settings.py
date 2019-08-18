c5e20293931a4a52046c8743812c93e6e1bc6357
maximus-sallam <maximus.sallam@gmail.com>
2019-08-18 07:03:58 -0400

    speed of aliens, ship, and bullet progressively get faster as you clear fleets

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings():
     """A class to store all settings for Max Invasion."""
 
     def __init__(self):
-        """Initialize the game's settings."""
+        """Initialize the game's static settings."""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
@@ -23,7 +23,24 @@ class Settings():
         self.bullets_allowed = 3
 
         # Alien settings
-        self.alien_speed_factor = 5
         self.fleet_drop_speed = 10
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed_factor = 15
+        self.bullet_speed_factor = 30
+        self.alien_speed_factor = 5
+
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
