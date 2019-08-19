632ce32456cdb0792c031188196f3862c14a4f6a
maximus-sallam <maximus.sallam@gmail.com>
2019-08-19 19:32:37 -0400

    added labels to scoreboard items

--- scoreboard.py
+++ scoreboard.py
@@ -22,7 +22,7 @@ class Scoreboard():
     def prep_score(self):
         """Turn the score into a rendered image."""
         rounded_score = int(round(self.stats.score, -1))
-        score_str = "{:,}".format(rounded_score)
+        score_str = "Score: " + "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True, self.text_color)
                                             # , self.ai_settings.bg_color)
 
@@ -40,7 +40,7 @@ class Scoreboard():
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
         high_score = int(round(self.stats.high_score, -1))
-        high_score_str = "{:,}".format(high_score)
+        high_score_str = "High Score: " + "{:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str, True,
                                                  self.text_color)
                                                  # , self.ai_settings.bg_color)
@@ -55,7 +55,7 @@ class Scoreboard():
 
     def prep_level(self):
         """Turn the level into a rendered image."""
-        self.level_image = self.font.render(str(self.stats.level), True,
+        self.level_image = self.font.render("Level: " + str(self.stats.level), True,
                                             self.text_color)
                                             # , self.ai_settings.bg_color)
 
