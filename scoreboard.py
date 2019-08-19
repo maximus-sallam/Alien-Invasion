9e46a49f9d41a202325f85e457405ed20fdcbf5e
maximus-sallam <maximus.sallam@gmail.com>
2019-08-19 07:07:26 -0400

    updated comment that remove background color on scoreboard

--- scoreboard.py
+++ scoreboard.py
@@ -21,8 +21,7 @@ class Scoreboard():
         """Turn the score into a rendered image."""
         score_str = str(self.stats.score)
         self.score_image = self.font.render(score_str, True, self.text_color)
-                                            # ,
-                                            # self.ai_settings.bg_color)
+                                            # , self.ai_settings.bg_color)
 
         # Display the score at the top right of the screen.
         self.score_rect = self.score_image.get_rect()
