7b05e55808869623989e959ed89205797ba9fe98
maximus-sallam <maximus.sallam@gmail.com>
2019-08-19 07:51:21 -0400

    removed print statement from increase_speed method in settings module

--- settings.py
+++ settings.py
@@ -51,4 +51,3 @@ class Settings():
         # self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
         self.alien_points = int(self.alien_points * self.score_scale)
-        print(self.alien_points)
